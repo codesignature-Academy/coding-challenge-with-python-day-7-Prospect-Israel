077be10ad3c218069599873cb926860a1b3c4332
Prospect Israel <prospectchiamaka38@gmail.com>
2025-11-20 21:07:04 +0100

    done

--- day7/assignment_day7.py
+++ day7/assignment_day7.py
@@ -1,11 +1,3 @@
-"""
-write four functions that perform the following tasks:
-add(a, b): returns the sum of a and b
-multiply(a, b): returns the product of a and b
-power(a, b): returns a raised to the power of b
-subtract(a, b): returns the difference of a and b
-"""
-
 
 
 def add(a,b):
@@ -31,6 +23,3 @@ def sub(g,h):
     return g - h
 sub_result = sub(34,21)
 print(sub_result)
-
-
-    
